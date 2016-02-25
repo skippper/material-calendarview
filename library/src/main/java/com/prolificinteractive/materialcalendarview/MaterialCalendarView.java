9f3f8a59f764a63e81522c08676dbffecf62bc8c
Norbert Sziráczki <norbert.sziraczki@odigeo.com>
2016-02-25 16:35:59 +0100

    Fixed tile size calculation

--- library/src/main/java/com/prolificinteractive/materialcalendarview/MaterialCalendarView.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/MaterialCalendarView.java
@@ -1580,8 +1580,8 @@ public class MaterialCalendarView extends ViewGroup {
             }
         } else if (specWidthMode == MeasureSpec.EXACTLY) {
             if (specHeightMode == MeasureSpec.EXACTLY) {
-                //Pick the larger of the two explicit sizes
-                measureTileSize = Math.max(desiredTileWidth, desiredTileHeight);
+                //Pick the smaller of the two explicit sizes so it will surely fit into the available space
+                measureTileSize = Math.min(desiredTileWidth, desiredTileHeight);
             } else {
                 //Be the width size the user wants
                 measureTileSize = desiredTileWidth;
